d2ae916ab8b00895f974544fc5c7f21adf2826e0
AugenAugen <allthestuffilove@protonmail.com>
2024-03-12 20:13:35 +0100

    Avalonia

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace DNDDice.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DNDDice.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
